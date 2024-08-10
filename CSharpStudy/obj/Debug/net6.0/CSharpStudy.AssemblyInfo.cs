b24eee00ab0147f983b28fd02da68b3462c673c4
GameLecture2024 <dlfjswodlf@naver.com>
2024-08-10 17:37:52 +0900

    C# 첫 프로그래밍 변수에 대한 이해

--- CSharpStudy/obj/Debug/net6.0/CSharpStudy.AssemblyInfo.cs
+++ CSharpStudy/obj/Debug/net6.0/CSharpStudy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b85c1a99aa168028806a863ba0c9d53f590465b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
